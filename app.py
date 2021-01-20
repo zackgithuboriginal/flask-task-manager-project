e8dcc7568046618e31a34abb37329732960c5848
zackgithuboriginal <z.gibbons@hotmail.com>
2021-01-20 15:49:37 +0000

    Create base template

--- app.py
+++ app.py
@@ -13,7 +13,9 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
+
 mongo = PyMongo(app)
+
 
 @app.route("/")
 @app.route("/get_tasks")

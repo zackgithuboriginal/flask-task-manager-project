f249d9edf19a03fdde9add6212b160ce02062417
zackgithuboriginal <z.gibbons@hotmail.com>
2021-01-21 14:49:02 +0000

    Add session user profile

--- app.py
+++ app.py
@@ -46,6 +46,7 @@ def register():
         # put the new user into 'session cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
     return render_template("register.html")
 
 
@@ -57,13 +58,15 @@ def login():
             {"username": request.form.get("username").lower()})
 
         if existing_user:
-            #ensure hased password matches user input
+            # ensure hased password matches user input
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
                     flash("Welcome, {}".format(request.form.get("username")))
+                    return redirect(
+                        url_for("profile", username=session["user"]))
             else:
-                #invalid password match
+                # invalid password match
                 flash("Incorrect Username and/or Password")
                 return redirect(url_for("login"))
 
@@ -72,6 +75,13 @@ def login():
             flash("Incorrect Username and/or Password")
             return redirect(url_for("login"))
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 if __name__ == "__main__":
